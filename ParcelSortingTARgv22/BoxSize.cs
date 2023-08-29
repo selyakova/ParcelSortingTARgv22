d29354065852b894e8b0f2ff0268d5f0ab0d46dc
opilane <opilane@HP-CZC0359NN5>
2023-08-29 15:48:06 +0300

    Add project files.

---
+++ ParcelSortingTARgv22/BoxSize.cs
@@ -0,0 +1,15 @@
+﻿namespace ParcelSortingTARgv22
+{
+    public class BoxSize
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public List<SortingLineParam> SortingLineParams { get; set; }
+        = new List<SortingLineParam>();
+    }
+    public class SortingLineParam
+    {
+        public int Size { get; set; }
+    }
+
+}
---
+++ ParcelSortingTARgv22/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace ParcelSortingTARgv22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            firstParcelLine(boxSizes);
+        }
+
+        static bool firstParcelLine(List<BoxSize> boxSizes)
+        {
+            bool parcelFits = false;
+            foreach (BoxSize box in boxSizes)
+            {
+                var boxLengthInHalf = box.Length / 2;
+                var halfBoxDiagonalNotSquare = (boxLengthInHalf * boxLengthInHalf) + (box.Width * box.Width);
+                var halfParcelDiagonal = Math.Sqrt(halfBoxDiagonalNotSquare);
+             foreach (SortingLineParam sortingLine in box.SortingLineParams)
+                {
+                    if (sortingLine.Size >= box.Width || sortingLine.Size >= box.Length) 
+                    {
+                      Console.WriteLine("Parcel fits to sorting line");  
+                    }
+                    if (sortingLine.Size >= halfParcelDiagonal) 
+                    {
+                        Console.WriteLine("Sorting line width is {0} and it fits", sortingLine.Size);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            return parcelFits;
+        }
+
+        public static readonly List<BoxSize> boxSizes = new List<BoxSize>
+        {
+            new BoxSize
+            {
+                Length = 120,
+                Width = 60,
+                SortingLineParams = new List<SortingLineParam>
+                {
+                    new SortingLineParam
+                    {
+                        Size = 100
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 75
+                    }
+                }
+            },
+             new BoxSize
+            {
+                Length = 100,
+                Width = 35,
+                SortingLineParams = new List<SortingLineParam>
+                {
+                    new SortingLineParam
+                    {
+                        Size = 75
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 50
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 80
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 100
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 37
+                    }
+                }
+            },
+            new BoxSize
+            {
+                Length = 70,
+                Width = 50,
+                SortingLineParams = new List<SortingLineParam>
+                {
+                    new SortingLineParam
+                    {
+                        Size = 60
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 60
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 55
+                    },
+                    new SortingLineParam
+                    {
+                        Size = 90
+                    }
+                }
+            }
+        };
+    }
+}
